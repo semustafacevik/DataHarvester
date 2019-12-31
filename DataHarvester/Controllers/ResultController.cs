6a040e654b6f97730cf241624e1879b941f674ed
MustafaCEVIK <cevikksoft@gmail.com>
2019-12-31 09:52:54 +0300

    Change template and logo.

---
+++ DataHarvester/Controllers/ResultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataHarvester.Controllers
+{
+    public class ResultController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Results()
+        {
+            return View();
+        }
+    }
+}
